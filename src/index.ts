1c4668c31279c131d3a29a1cc7c0bf2428997869
gpaez-ol <tavo@outlandhq.com>
2023-04-23 21:55:48 -0600

    feat: Added  Instructions, Declarations and basic assignment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import * as parser from "parser";
 // TODO: Switch for a file
-const code: string = "2+2";
+const code: string = "int varB = 4; int varC = 8 + varB;";
 
 const compileBtn = document.getElementById("runtimeBtn");
+onload = () => {
+  try {
+    const result = parser.parse(code);
+    console.log("Analisis correcto", result);
+  } catch (error) {
+    console.log(error);
+    console.log("Error al analizarse");
+  }
+};
 compileBtn?.addEventListener("click", () => {
   try {
     const result = parser.parse(code);
     console.log("Analisis correcto", result);
-  } catch {
+  } catch (error) {
+    console.log(error);
     console.log("Error al analizarse");
   }
 });
