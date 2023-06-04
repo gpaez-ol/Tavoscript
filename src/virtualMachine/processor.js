dd45118c96074f432880fc5b2ea5fe9664e9722e
gpaez-ol <tavo@outlandhq.com>
2023-06-04 14:20:00 -0500

    feat: Started using Code Segment instead of quadruples directly

--- src/virtualMachine/processor.js
+++ src/virtualMachine/processor.js
@@ -1,12 +1,13 @@
-const { startVariablesMemory,memory } = require("./memoryUtils");
+const { startVariablesMemory,loadFunction,memory } = require("./memoryUtils");
 const { solveOperation} = require("./operationUtils");
+const {getFunctionQuadruples} = require("./functionUtils");
 async function startProgram({quadruples,functions},devMode)
 {
     console.log("Starting Process ------------------------")
-    startVariablesMemory(functions,devMode);
     let currentQuadruple = 0;
     let globalQuads = false;
     let globalQuadruples = quadruples.filter(quadruple => quadruple.global === true);
+    startVariablesMemory(functions,globalQuadruples,devMode);
     if(devMode)
     {
         console.log("globalQuads",globalQuadruples);
@@ -14,6 +15,9 @@ async function startProgram({quadruples,functions},devMode)
     if(globalQuadruples.length > 0)
     {
         globalQuads = true;
+    }else {
+        let mainFunctionQuadruples = getFunctionQuadruples(functions[1].quadruplesStart,quadruples);
+        loadFunction(functions[1],[],mainFunctionQuadruples);
     }
     while(currentQuadruple !== undefined && currentQuadruple !== null)
     {
@@ -24,6 +28,8 @@ async function startProgram({quadruples,functions},devMode)
             globalQuads = false;
             currentQuadruple=0;
             console.log("Global Quads finished")
+            let mainFunctionQuadruples = getFunctionQuadruples(functions[1].quadruplesStart,quadruples);
+            loadFunction(functions[1],[],mainFunctionQuadruples);
         }else if(solvedOperation.currentQuadruple !== undefined)
         {
             currentQuadruple = solvedOperation.currentQuadruple;
